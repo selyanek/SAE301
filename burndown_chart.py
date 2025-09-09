d29a969cba70140bb7f8d345665d72c93f21e97c
selyanek <selyane.khentache@uphf.fr>
2025-09-09 08:59:05 +0200

    Create burndown_chart.py

---
+++ burndown_chart.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# BURNDOWN CHART
+# pourra être modifié en fonction de notre avancement
+
+if __name__ == '__main__':
+
+    ideal = [59,50,30,0]
+    real = [59,59,43,24]
+    sprint = [0,1,2,3]
+
+    plt.plot(sprint, ideal, label='Avancement idéal', color='red', linestyle='--')
+    plt.plot(sprint, real, label='Avancement réel', color='blue', linestyle='--')
+
+    plt.xlabel('Sprint')
+    plt.ylabel('Story Points')
+    plt.legend()
+
+    # Forcer des ticks de 1 en 1 sur l'axe X
+    plt.xticks(range(min(sprint), max(sprint) + 1, 1))
+
+
+
+
+    plt.title('Burndown chart pour équipe 1')
+
+    plt.show()
