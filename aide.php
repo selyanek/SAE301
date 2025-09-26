ea04a980baabd049157607126c1161be7f7eb6df
selyanekhentache <selyane.khentache@uphf.fr>
2025-09-26 11:00:15 +0200

    page aide

---
+++ aide.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Aide</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+<h1>Aide </h1>
+<p>EduTrack est l’application officielle de l’IUT qui permet aux étudiants de justifier leurs absences en ligne. Plus besoin de passer au secrétariat : tout se fait directement depuis la plateforme.</p>
+
+<h2>Comment ça marche ?</h2>
+
+<ol>
+    <li>Déclare ton absence (Remplis un formulaire)</li>
+    <li>Téléverse ton justificatif (certificat, convocation, etc...)</li>
+    <li>Attends la validation du responsable</li>
+</ol>
+
+<a href="https://moodle.uphf.fr/course/view.php?id=3785">Règlement intérieur sur les absences</a>
+
+<p>Pensez à transmettre vos justificatifs dans les délais afin que vos absences soient correctement prises en compte.</p>
+
+<a href="accueil_etudiant.php"><button type="button">Retour à l'accueil</button></a>
+
+</body>
+</html>
