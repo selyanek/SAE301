905037b02f82e7d6f4cc50bc73cf0e84d5d6b2e8
baptisteChouraih <Baptiste.Chouraih@uphf.fr>
2025-09-26 11:38:04 +0200

    connexion bdd

--- add_absence_to_db.php
+++ add_absence_to_db.php
@@ -1,21 +1,16 @@
 <?php
-// Paramètres de connexion à la base de données
-$host = 'node2.liruz.fr';
-$port = '5435';
-$dbname = 'sae';
-$username = 'sae';
-$password = '1zevkN&49b&&a*Pi97C';
-
+require 'Database.php';
 try {
-    // Création de la connexion PDO
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
-    $pdo = new PDO($dsn, $username, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ]);
+    $db = new Database();
+    $pdo = $db->getConnection();
     
-    echo "Connexion à la base de données réussie !";
+    $sql = "SELECT * FROM etudiant";
+
+    foreach ($pdo->query($sql) as $row) {
+        echo $row['id'] . ' - ' . $row['nom'] . ' ' . $row['prenom'] . ' - ' . $row['formation'] . '<br>';
+    }
+
+    $pdo = null;
     
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
@@ -24,4 +19,3 @@ try {
 
 // Fermeture de la connexion
 $pdo = null;
-?>
