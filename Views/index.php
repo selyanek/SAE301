4e41acb1a77418af9fed6e7d9be0c5a21ed68ee8
Dilara Simsek <dilarasimsek@MacBook-Air-de-Dilara.local>
2025-10-02 10:08:39 +0200

    Changement du css de la page de connexion, ajout de logo connexion, gestion_absence renommer en gererAbsEtu

--- Views/index.php
+++ Views/index.php
@@ -5,23 +5,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style2.css" rel="stylesheet">
+    <link href="/Views/style2.css" rel="stylesheet">
     <title>Login</title>
 </head>
+<header class="header">
+    <img src="../img/logouphf.png" alt="Logo uphf">
+</header>
 <body>
-    <header>
-        <h1>Bienvenue sur le portail d'authentification</h1>
-    </header>
+    <div class="logoEdu">
+        <img src="../img/logoedutrack.png" alt="Logo EduTrack">
+    </div>
+    <div class="separator"></div>
+    <div class="text">
+         <h1>Bienvenue sur le portail d'authentification</h1>
+    </div>
+    <div class="text-with-image">
+        <img src="../img/logoco.png" alt="Connexion">
+        <h2>Connexion</h2>
+    </div>
+
+    <div class="sidebar"></div>
 
     <div class="wapper">
         <form action="index.php" method="post">
             <label for="identifiant">Identifiant :</label>
             <input type="text" id="identifiant" name="identifiant">
-
+            <br>
             <label for="mot_de_passe">Mot de passe :</label>
             <input type="password" id="mot_de_passe" name="mot_de_passe">
-
+            <br>
             <button type="submit">Se connecter</button>
+            <br>
             <a href="mdpOublier.php">Mot de passe oublié ?</a>
         </form>
     </div>
