b62931fc79a4eabbac77581c1ec4cb48b663be54
baptisteChouraih <Baptiste.Chouraih@uphf.fr>
2025-10-13 16:43:39 +0200

    FIX redirection + session

--- Controllers/accueil_prof.php
+++ Controllers/accueil_prof.php
@@ -1,11 +1,40 @@
+<?php
+session_start()
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>accueil prof</title>
+    <link href="/CSS/cssDeBase.css" rel="stylesheet">
 </head>
+<div class="uphf">
+    <img src="../img/logouphf.png" alt="Logo uphf">
+</div>
 <body>
-    <h1>vous etes prof</h1>
+<div class="logoEdu">
+    <img src="../img/logoedutrack.png" alt="Logo EduTrack">
+</div>
+<div class="sidebar">
+    <ul>
+        <li><a href="../Controllers/accueil_etudiant.php">Accueil</a></li>
+        <li><a href="../Views/gererAbsEtu.php">Gérer des absences</a></li>
+        <li><a href="#">Historique des absences</a></li>
+        <li><a href="../Views/aide.php">Aides</a></li>
+    </ul>
+</div>
+
+<header class="text">
+    <h1> Bonjour, <?php echo $_SESSION['identifiant'] ?> </h1>
+    <a href="../Views/gererAbsEtu.php"><button type="submit" class="btn">Consulter vos justificatifs</button></a>
+</header>
+<footer class="footer">
+    <nav class="footer-nav">
+        <a href="/Controllers/accueil_etudiant.php">Accueil</a>
+        <span>|</span>
+        <a href="../Views/aide.php">Aides</a>
+    </nav>
+</footer>
 </body>
 </html>
