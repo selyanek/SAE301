c11286d15c602263abecfdbd8542297e029269ee
baptisteChouraih <Baptiste.Chouraih@uphf.fr>
2025-10-14 00:15:28 +0200

    ADD (Redirect) la class redict qui ne c est pas push

---
+++ Models/Redirect.php
@@ -0,0 +1,29 @@
+<?php
+class Redirect
+{
+    private $roleNecessaire;
+    public function __construct($roleNecessaire){
+        $this->roleNecessaire = $roleNecessaire;
+    }
+    public function redirect(){
+        if (!isset($_SESSION['login']) || !isset($_SESSION['role'])) {
+            header('Location:../Views/index.php');
+            exit();
+        }if ($_SESSION['role'] !== $this->roleNecessaire) {
+            switch ($_SESSION['role']) {
+                case 'etudiante':
+                    header('Location: accueil_etudiant.php');
+                    exit();
+                case 'professeur':
+                    header('Location: accueil_professeur.php');
+                    exit();
+                case 'responsable_pedagogique':
+                    header('Location: accueil_responsable_pedagogique.php');
+                    exit();
+                default:
+                    header('Location: index.php');
+                    exit();
+            }
+        }
+    }
+}
