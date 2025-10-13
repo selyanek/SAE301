d5fa2f4de3fbb381535e0b52e08310b635903224
selyanekhentache <selyane.khentache@uphf.fr>
2025-10-13 11:05:53 +0200

    message erreur si dépasse 48h ou date invalide + classe pour récupérer données des absences

---
+++ Controllers/get_absence_from_db.php
@@ -0,0 +1,24 @@
+<?php
+// get_id.php
+// (à compléter)
+// Ce script récupère toutes les informations d'absence.
+require 'Database.php';
+
+class Get_Absence {
+
+
+    public function fetchAbsences(): array|string {
+
+        try {
+            $db = new Database();
+            $pdo = $db->getConnection();
+            $sql = 'SELECT * FROM Absence WHERE justifie = FALSE ORDER BY date_debut DESC';
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $result;
+        } catch (PDOException $e) {
+            return "Erreur : " . $e->getMessage();
+        }
+    }
+}
