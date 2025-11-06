c505a5318083feba3a1fc684e3933ae05d189eb7
baptisteChouraih <Baptiste.Chouraih@uphf.fr>
2025-11-06 23:11:14 +0100

    FIX (probleme merge arbo)

--- src/Views/layout/header.php
+++ src/Views/layout/header.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion des Absences</title>
-    <link rel="stylesheet" href="../../CSS/cssDeBase.css">
+    <link rel="stylesheet" href="../../../public/asset/CSS/cssDeBase.css">
 </head>
 <body>
 <div class="uphf">
-    <img src="../../img/logouphf.png" alt="Logo uphf">
+    <img src="../../../public/asset/img/logouphf.png" alt="Logo uphf">
 </div>
 <div class="logoEdu">
-    <img src="../../img/logoedutrack.png" alt="Logo EduTrack">
+    <img src="../../../public/asset/img/logoedutrack.png" alt="Logo EduTrack">
 </div>
