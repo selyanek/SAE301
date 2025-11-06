d728dafe090c273c35e0b5a8739f8a0c6934cd35
baptisteChouraih <Baptiste.Chouraih@uphf.fr>
2025-11-06 22:55:09 +0100

    temp (gestion ABS)

--- src/Controllers/absenceController.php
+++ src/Controllers/absenceController.php
@@ -1,9 +1,40 @@
 <?php
+
+namespace src\Controllers;
+
 use src\Models\Absence;
 use src\Database\Database;
 
-$db = new Database();
-$pdo = $db->getConnection();
 
-$absence = new Absence($pdo);
+class AbsenceController
+{
+     private $absence;
+
+    public function __construct() {
+        $db = new Database();
+        $this->absence = new Absence($db->getConnection());
+    }
+
+    public function CreerAbsence() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->absence->setIdCours($_POST['idCours']);
+            $this->absence->setIdEtudiant($_POST['idEtudiant']);
+            $this->absence->setDateDebut($_POST['date_debut']);
+            $this->absence->setDateFin($_POST['date_fin']);
+            $this->absence->setMotif($_POST['motif'] ?? null);
+            $this->absence->setJustifie(isset($_POST['justifie']) ? (bool)$_POST['justifie'] : false);
+            $this->absence->setUriJustificatif($_POST['uriJustificatif'] ?? null);
+
+            $result = $this->absence->ajouterAbsence();
+
+            if ($result) {
+                echo json_encode(['message' => 'Absence ajoutée avec succès', 'id' => $result]);
+            } else {
+                echo json_encode(['error' => 'Erreur lors de l’ajout de l’absence']);
+            }
+        }
+    }
+}
+
+
 
