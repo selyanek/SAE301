fca55dcc21b7bf509eea9277b862d90ddcbb115b
selyanekhentache <selyane.khentache@uphf.fr>
2025-11-18 16:48:36 +0100

    commence traitement CSV pour filtrer les absences évals

---
+++ src/Models/evals.php
@@ -0,0 +1,21 @@
+
+<?php require_once __DIR__ .'src/Models/GestionCSV.php';
+
+// TODO
+
+// FIX LES PATH
+// finirai demain
+
+$csv = new GestionCSV();
+$result = [];
+$csv_array = $csv->getAllData("data/CSV/BUT1-240122-240223_anonymise.CSV");
+
+// FILTRER EVALS
+foreach ($csv_array as $row) {
+    if ($row[23] == "Oui") {
+        $result += ($row);
+    }
+}
+
+echo implode("," , $result);
+?>
