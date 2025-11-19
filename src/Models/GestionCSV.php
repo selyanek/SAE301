baaf04a214c9ff4f4aafee7ec52918a10154d10d
selyanekhentache <selyane.khentache@uphf.fr>
2025-11-19 10:12:55 +0100

    commencer export csv vers db

--- src/Models/GestionCSV.php
+++ src/Models/GestionCSV.php
@@ -2,6 +2,8 @@
 
 namespace src\Models;
 use Exception;
+use PDO;
+use src\Database\Database;
 
 // Classe pour gérer la lecture et l'affichage de fichiers CSV
 class GestionCSV
@@ -46,5 +48,59 @@ class GestionCSV
         }
     }
 
+    public function exportToDB($files, Database $database): ?string
+    {
+        $csv_array = $this->getAllData($files);
+        $pdo = $database->getConnection(); 
+
+        foreach ($csv_array as $row) {
+            $motif = $row[18];
+            $justifie = $row[17] !== "Non justifié"; // Booléen si justifié ou non
+
+            // TODO: implementer la logique pour trouver l'id_cours et l'id_etudiant
+            // pour l'instant, id mis à 0
+
+            $id_cours = 0;
+            $id_etudiant = 0;
+
+            $d = $this->getDate($row[9], $row[10]); // recup string heure et date du CSV
+            $date_start = \date('Y-m-d H:i:s', $d);
+            $date_end = \date('Y-m-d H:i:s', $d + $this->convertToSeconds($row[11]));
+
+            $sql = "INSERT INTO Absence (date_debut, date_fin, motif, justifie) 
+                    VALUES (:date_debut, :date_fin, :motif, :justifie)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([
+                ':date_debut' => $date_start,
+                ':date_fin' => $date_end,
+                ':motif' => $motif,
+                ':justifie' => $justifie ? 1 : 0 // Convert boolean to int for DB
+            ]);
+        }
+        return $pdo->lastInsertId();
+    }
+
+    private function convertToSeconds(string $str): int
+    {
+        return match($str) { // R
+            '1H30' => 5400,
+            '3H00' => 10800,
+            default => 0
+        };
+    }
+
+    private function getDate(string $date, string $heure): int
+    {
+        $jour = \intval(\substr($date, 0, 2));
+        $mois = \intval(\substr($date, 3, 2));
+        $annee = \intval(\substr($date, 6, 4));
+
+        $heures = \strlen($heure) === 4 
+            ? \intval(\substr($heure, 0, 1)) 
+            : \intval(\substr($heure, 0, 2));
+        $minutes = \intval(\substr($heure, \strlen($heure) === 4 ? 2 : 3, 2));
+
+        return \mktime($heures, $minutes, 0, $mois, $jour, $annee);
+    }
 }
 ?>
