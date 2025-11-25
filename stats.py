4044a20e01d8287860d325d17aeff4613c759084
selyanekhentache <selyane.khentache@uphf.fr>
2025-11-25 15:38:18 +0100

    stats++

--- stats.py
+++ stats.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import psycopg
+import datetime
 
 
 
@@ -33,24 +34,6 @@ def repartition_absences_par_heure(nb : list[int]):
 
     plt.bar(cours, nb, color=couleurs)
 
-    plt.title('Répartition des absences en fonction du type de cours')
-    plt.xlabel('Heure de la journée', fontweight ='bold', fontsize = 15)
-    plt.ylabel("Nombre d'absences", fontweight ='bold', fontsize = 15)
-
-    # Sauvegarder l'image sous format png pour pouvoir l'afficher ultérieurement sur le site
-    
-    plt.savefig("absences2.png")
-    plt.show()
-
-def repartition_absences_par_heure(nb : list[int]):
-    
-    # la liste nb est censée contenir 6 éléments : le nombre d'absences aux heures correspondantes
-    
-    cours = ['8h-9h30', '9h30-11h', '11h-12h30', '14h-15h30', '15h30-17h', '17h-18h30']
-    couleurs = ["#001BB4", "#006DB6", "#0090D3", "#00CAEE","#00F0E4", "#00FFB3"]
-
-    plt.bar(cours, nb, color=couleurs)
-
     plt.title("Répartition des absences en fonction de l'heure")
     plt.xlabel('Heure de la journée', fontweight ='bold', fontsize = 15)
     plt.ylabel("Nombre d'absences", fontweight ='bold', fontsize = 15)
@@ -59,8 +42,28 @@ def repartition_absences_par_heure(nb : list[int]):
     
     plt.savefig("absences2.png")
     plt.show()
+
+def absences_14_derniers_jours(nb : list[int]):
+    
+    # la liste nb est censée contenir 6 éléments : le nombre d'absences / jour les 14 derniers jours
+
+    plt.plot(nb, color = 'black', linestyle = '-')
+
+    plt.title("Récapitulatif d'absences sur les 14 derniers jours")
+    plt.xlabel('Jour', fontweight ='bold', fontsize = 15)
+    plt.ylabel("Nombre d'absences", fontweight ='bold', fontsize = 15)
+
+    # Sauvegarder l'image sous format png pour pouvoir l'afficher ultérieurement sur le site
+
+    
+    plt.grid(True)
+    
+    plt.savefig("absences3.png")
+    plt.show()
     
 if __name__ == "__main__":
     repartition_absences_par_cours([200,300,400,500])
     repartition_absences_par_heure([200,300,400,500,600,700])
+    absences_14_derniers_jours([1,2,5,3,6,15,1,5,1,4,1,3,10,11])
+
 
