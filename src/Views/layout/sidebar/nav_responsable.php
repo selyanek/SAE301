e75f724e843617c5a0868bc295ea6d5d1b03f174
MinRold <156063275+MinRold@users.noreply.github.com>
2025-12-12 15:17:34 +0100

    changement redirection

--- src/Views/layout/sidebar/nav_responsable.php
+++ src/Views/layout/sidebar/nav_responsable.php
@@ -2,10 +2,10 @@
 
 <div class="sidebar">
     <ul>
-        <li><a href="/src/Views/responsable/dashbord.php"> Accueil</a></li>
+        <li><a href="/src/Views/accueil_responsable.php"> Accueil</a></li>
         <li><a href="/src/Controllers/profile.php"> Mon profil</a></li>
-        <li><a href="/src/Views/responsable/gestionAbsence.php"> Gestion des absences</a></li>
-        <li><a href="/src/Views/responsable/historiques.php"> Historique des absences</a></li>
+        <li><a href="/src/Views/gestionAbsResp.php"> Gestion des absences</a></li>
+        <li><a href="/src/Views/historiqueAbsResp.php"> Historique des absences</a></li>
         <li><a href="/src/Views/responsable/statistiques.php"> Statistiques</a></li>
     </ul>
 </div>
